8e6dd9b184f07cfa27fca677d0b5f1603183802f
Ahmed <apop319@gmail.com>
2022-02-28 02:53:28 +0200

    added:  -Drag and drop  -Bulk edit  -Bulk edit Interface  -Selection count track fixed:  -Bugs in Table remove  -Changed Table columns width

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 import sys
 import time
+
+from matplotlib import artist
 import resources_rc
 import eyed3
 import webbrowser
@@ -9,7 +11,7 @@ from PyQt5 import uic
 from PyQt5.QtWidgets import QFileDialog
 from PyQt5 import QtWidgets
 from PyQt5.QtGui import QPixmap, QIcon
-from PyQt5.QtCore import Qt, QEvent, QPoint
+from PyQt5.QtCore import Qt, QEvent, QPoint, QPersistentModelIndex, QModelIndex
 from ImageViewer import QImageViewer
 
 repo = "https://github.com/Ahm3dRN/TagEditor"
@@ -18,6 +20,7 @@ class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
         QtWidgets.QMainWindow.__init__(self)
         self.ui = uic.loadUi("newui.ui", self)
+        self.bulk_ui = uic.loadUi("bulk_dialog.ui")
         self.setWindowFlag(Qt.FramelessWindowHint)
         self.installEventFilter(self)
         self.frame_8.installEventFilter(self)
@@ -26,6 +29,16 @@ class MainWindow(QtWidgets.QMainWindow):
         self.playlist_table.sortByColumn(1, Qt.AscendingOrder)
         self.playlist_table.setSelectionBehavior(QtWidgets.QTableView.SelectRows)
         self.selectionModel = self.playlist_table.selectionModel()
+        self.playlist_table.horizontalHeader().resizeSection(0, 350)
+        self.playlist_table.horizontalHeader().resizeSection(1, 250)
+
+
+        self.playlist_table.setAcceptDrops(True)
+        self.playlist_table.viewport().installEventFilter(self)
+        types = ['text/uri-list']
+        types.extend(self.playlist_table.mimeTypes())
+        self.playlist_table.mimeTypes = lambda: types
+
         sizegrip = QtWidgets.QSizeGrip(self)
         sizegrip.setMinimumSize(8,8)
         sizegrip.setMaximumSize(8,8)
@@ -53,7 +66,69 @@ class MainWindow(QtWidgets.QMainWindow):
         self.exit_button.clicked.connect(self.close)
         self.menu_button.clicked.connect(self.menuClicked)
         self.github.clicked.connect(self.open_repo)
+        self.bulk_edit.clicked.connect(self.show_bulk)
+        self.bulk_ui.bulk_save_input.clicked.connect(self.save_bulk_data)
     
+    def save_bulk_data(self):
+        selected_items = self.selectionModel.selectedRows()
+        selected_items.reverse()
+        if len(selected_items) > 1:
+            print("Are you Sure to bulk edit?")
+            qm = QtWidgets.QMessageBox()
+            ret = qm.question(self,'', "Are you sure to edit all selected items?", qm.Yes | qm.No)
+            if ret == qm.Yes:
+                for item in selected_items:
+                    it = self.playlist_table.item(item.row(), 0)
+                    path = it.text()
+                    print(path)
+                    tag = id3.Tag()
+                    tag.parse(path)
+                    self._save_bulk(tag)
+                    self.playlist_table.removeRow(item.row())
+                    self.handle_new(path, source="open")
+
+                self.show_info("Saved")
+            else:
+                pass
+            self.bulk_ui.close()
+
+    def _save_bulk(self, tag):
+        
+        if self.bulk_ui.bulk_title_box.isChecked():
+            title = self.bulk_ui.bulk_title_input.text()
+            tag.title = title
+
+        if self.bulk_ui.bulk_artist_box.isChecked():
+            artist = self.bulk_ui.bulk_artist_input.text()
+            tag.artist = artist
+        
+        if self.bulk_ui.bulk_album_box.isChecked():
+            album = self.bulk_ui.bulk_album_input.text()
+            tag.album = album
+
+        if self.bulk_ui.bulk_album_artist_box.isChecked():
+            album_artist = self.bulk_ui.bulk_album_input.text()
+            tag.album_artist = album_artist
+
+        if self.bulk_ui.bulk_author_box.isChecked():
+            author = self.bulk_ui.bulk_author_input.text()
+            tag.author = author
+
+        if self.bulk_ui.bulk_genre_box.isChecked():
+            genre = self.bulk_ui.bulk_genre_input.text()
+            tag.genre = genre
+
+        if self.bulk_ui.bulk_track_box.isChecked():
+            track = self.bulk_ui.bulk_track_input.text()
+            tag.track = track
+
+        if self.bulk_ui.bulk_year_box.isChecked():
+            year = self.bulk_ui.bulk_year_input.text()
+            tag.year = year
+        tag.save()
+    
+    def show_bulk(self):
+        self.bulk_ui.show()
     
     def open_repo(self):
         webbrowser.open(repo)
@@ -83,6 +158,16 @@ class MainWindow(QtWidgets.QMainWindow):
             delta = QPoint(event.globalPos() - self.oldPos)
             self.move(self.x() + delta.x(), self.y() + delta.y())
             self.oldPos = event.globalPos()
+        if (event.type() == QEvent.Drop and
+            event.mimeData().hasUrls()):
+            print(event.mimeData().urls())
+            for url in event.mimeData().urls():
+                # self.addFile(url.toLocalFile())
+                print(url)
+                print(url.toLocalFile())
+                if url.toLocalFile().split(".")[-1] == "mp3":
+                    self.handle_new(url.toLocalFile(), "open")
+            return True
         return QtWidgets.QMainWindow.eventFilter(self, obj, event)
 
     def menuClicked(self):
@@ -99,6 +184,9 @@ class MainWindow(QtWidgets.QMainWindow):
     
     def handle_table_remove(self):
         selected_items = self.selectionModel.selectedRows()
+        print(type(selected_items))
+        # We reverse  to avoid row conflicts after deleting each row
+        selected_items.reverse()
         for item in selected_items:
             self.playlist_table.removeRow(item.row())
 
@@ -170,6 +258,8 @@ class MainWindow(QtWidgets.QMainWindow):
             self.update_cover(ncover)
     
     def selChanged(self, a):
+        current_selection = len(self.selectionModel.selectedRows())
+        self.selected_count.setText(f"Selected {current_selection}/{self.playlist_table.rowCount()}")
         if len(a.indexes()) > 0:
             it = self.playlist_table.item(a.indexes()[0].row(), 0)
             path = it.text()
@@ -183,8 +273,10 @@ class MainWindow(QtWidgets.QMainWindow):
 
     def save_lyrics_form(self):
         lyrics = self.lyrics_input.toPlainText()
+        comments = self.comments_input.toPlainText()
         if self.current_tag:
             self.current_tag.lyrics.set(lyrics)
+            self.current_tag.comments.set(comments)
             self.current_tag.save(version=eyed3.id3.ID3_DEFAULT_VERSION, encoding='utf-8')
             self.show_info("Saved")
         else:
@@ -197,8 +289,14 @@ class MainWindow(QtWidgets.QMainWindow):
         if not self.current_tag:
             self.show_unloaded_warning()
             return
+
         tag = self.current_tag
         a = self.current_a
+        self._save(tag)
+        self.show_info("Saved")
+        
+
+    def _save(self, tag):
         title = self.title_input.text()
         artist = self.artist_input.text()
         album = self.album_input.text()
@@ -211,12 +309,11 @@ class MainWindow(QtWidgets.QMainWindow):
         tag.album = album
         tag.title=title
         tag.album_artist = album_artist
-        tag.track_num = int(track)
-        tag.year = int(year)
+        tag.track_num = int(track) if track.isnumeric() else None
+        tag.year = year
         tag.genre = genre
         tag.save(version=eyed3.id3.ID3_DEFAULT_VERSION, encoding='utf-8')
-        self.show_info("Saved")
-
+        
     def add_entry(self, row: int, data: list):
         """
         Filename, Title, Artist, Album, track#, Year, Genre
@@ -250,8 +347,13 @@ class MainWindow(QtWidgets.QMainWindow):
         tag = id3.Tag()
         tag.parse(path)
         a = load(path)
-        if (a.tag == None):
-            a.initTag()
+        print(a)
+        print(tag)
+        if a:
+            if (a.tag == None):
+                a.initTag()
+        else:
+            return
 
         self.current_tag = tag
         self.current_a = a
@@ -267,9 +369,9 @@ class MainWindow(QtWidgets.QMainWindow):
         year = ""
         genre = ""
         lyrics = ""
+        comments = ""
         if tag.title:
             title = tag.title
-            self.set_title(title)
         imgs = a.tag.images
         if len(imgs) > 0:
             for image in a.tag.images:
@@ -291,6 +393,9 @@ class MainWindow(QtWidgets.QMainWindow):
         
         if tag.track_num:
             track = f"{tag.track_num[0]}"
+            if track == "None":
+                print("track is none")
+                track = ""
         
         if a.info.bit_rate:
             bit_rate = f"{a.info.bit_rate}, {a.info.bit_rate_str}"
@@ -306,7 +411,10 @@ class MainWindow(QtWidgets.QMainWindow):
         
         if tag.lyrics:
             lyrics = tag.lyrics[0].text
+        if tag.comments:
+            comments = tag.comments[0].text
 
+        self.set_title(title)
         self.set_artist(artist)
         self.set_album(album)
         self.duration_input.setText(duration)
@@ -317,6 +425,7 @@ class MainWindow(QtWidgets.QMainWindow):
         self.set_year(year)
         self.set_genre(genre)
         self.lyrics_input.setText(lyrics)
+        self.comments_input.setText(comments)
         self.access_date_input.setText(f"{time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(tag.file_info.atime))}")
         self.modified_date_input.setText(f"{time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(tag.file_info.mtime))}")
         
